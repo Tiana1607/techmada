79a6e544eb25b51be716a751cd69f51c73b8d9ef
Tiana1607 <rovarajaonarimanana@gmail.com>
2026-05-13 09:25:53 +0300

    bdd init

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -57,7 +57,7 @@ class Database extends Config
     //     * @var array<string, mixed>
     //     */
        public array $default = [
-           'database'    => 'database.db',
+           'database'    => WRITEPATH . 'database/app.db',
            'DBDriver'    => 'SQLite3',
            'DBPrefix'    => '',
            'DBDebug'     => true,
